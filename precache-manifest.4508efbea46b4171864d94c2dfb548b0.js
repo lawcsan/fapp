aaa6bcec887cc1dc163bcb6df877578ae0c578e9
lawcsan <lawcsan@gmail.com>
2019-09-22 23:20:06 +0500

    Updates

---
+++ precache-manifest.4508efbea46b4171864d94c2dfb548b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7263b1321b7d3727ae1788ed4a62463b",
+    "url": "/fapp/index.html"
+  },
+  {
+    "revision": "c8332fc4f8e591f6523f",
+    "url": "/fapp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a9965f56136985d75a3a",
+    "url": "/fapp/static/css/main.48330483.chunk.css"
+  },
+  {
+    "revision": "c8332fc4f8e591f6523f",
+    "url": "/fapp/static/js/2.3e05445e.chunk.js"
+  },
+  {
+    "revision": "a9965f56136985d75a3a",
+    "url": "/fapp/static/js/main.88f661bb.chunk.js"
+  },
+  {
+    "revision": "474de6f881fea7f355d0",
+    "url": "/fapp/static/js/runtime-main.0c4fa571.js"
+  },
+  {
+    "revision": "4c51214613dc35465553e71e131a4cb3",
+    "url": "/fapp/static/media/logo.4c512146.svg"
+  }
+]);
